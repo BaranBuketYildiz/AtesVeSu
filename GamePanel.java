a8dba8a92e7b98aec36d3ccadec66e3e04b42921
scgursel <servetcan99@outlook.com>
2024-03-31 20:16:51 +0300

    Auto commit

--- GamePanel.java
+++ GamePanel.java
@@ -1,30 +1,70 @@
 import java.awt.*;
+
 import javax.swing.*;
 
+import java.awt.event.*;
 
-
-public class GamePanel extends JPanel{
-     
-
-
+public class GamePanel extends JPanel implements KeyListener {
 
     Player p1;
+    Player p2;
 
-    public GamePanel(){
+
+    public GamePanel() {
         setPreferredSize(new Dimension(900, 1200));
+        this.addKeyListener(this);
+        p1 = new Player(0, 0, 100, 75, Color.red);
+        p2 = new Player(0, 0, 100, 75, Color.blue);
 
-         p1 = new Player(0, 0, 100, 75, Color.red);
+        setFocusable(true);
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int keyCode = e.getKeyCode();
+        System.out.println("sdasdasd");
+        switch (keyCode) {
+            case KeyEvent.VK_W:
+                p1.setY(p1.getY() - 10);
+                repaint();
+                break;
+            case KeyEvent.VK_A:
+                p1.setX(p1.getX() - 10);
+                repaint();
+                break;
+
+            case KeyEvent.VK_S:
+                p1.setY(p1.getY() + 10);
+                repaint();
+                break;
+            case KeyEvent.VK_D:
+                p1.setX(p1.getX() + 10);
+                repaint();
+                break;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
 
     }
-    
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
     @Override
     protected void paintComponent(Graphics g) {
-            // TODO Auto-generated method stub
-            super.paintComponent(g);
+        // TODO Auto-generated method stub
+        super.paintComponent(g);
 
-            g.setColor(p1.getColor());
-            g.fillRect(p1.getX(), p1.getY(), p1.getWidth(), p1.getHeight());
+        g.setColor(p1.getColor());
+        g.fillRect(p1.getX(), p1.getY(), p1.getWidth(), p1.getHeight());
     }
-   
+
+ 
 
 }
--- Player.java
+++ Player.java
@@ -4,9 +4,7 @@ import java.awt.event.*;
 
 import javax.swing.*;
 
-
-public class Player extends JFrame implements KeyListener {
-
+public class Player extends JComponent {
 
         private Color color;
         private int x;
@@ -14,48 +12,16 @@ public class Player extends JFrame implements KeyListener {
         private int height;
         private int width;
 
-        @Override
-        public void keyPressed(KeyEvent e) {
-        // TODO Auto-generated method stub
-        int keyCode=e.getKeyCode();
-
-        switch(keyCode){
-             
-            case KeyEvent.VK_W:
-            System.out.println("w");
-                setY(getY()+10);
-                break;
-            case KeyEvent.VK_A:
-            System.out.println("a");
-                setX(getX()-10);
-                break;      
-            case KeyEvent.VK_D:
-            System.out.println("D");
-                 setX(getX()+10);
-                  break;
-        }
-        
-        }
-        @Override
-        public void keyReleased(KeyEvent e) {
-        // TODO Auto-generated method stub
-        
-        }
-        @Override
-        public void keyTyped(KeyEvent e) {
-        // TODO Auto-generated method stub
-        
-        }
-        public Player(int x, int y, int height, int width, Color c){
+      
+        public Player(int x, int y, int height, int width, Color c) {
                 this.x = x;
                 this.y = y;
                 this.height = height;
                 this.width = width;
                 this.color = c;
+                setFocusable(true);
 
-                addKeyListener(this);
         }
-
 
         @Override
         public void paintComponents(Graphics g) {
@@ -64,59 +30,42 @@ public class Player extends JFrame implements KeyListener {
                 g.setColor(color);
                 g.fillRect(x, y, width, height);
         }
+
         public Color getColor() {
                 return color;
         }
-
-
 
         public void setColor(Color color) {
                 this.color = color;
         }
 
-
-
         public int getX() {
                 return x;
         }
-
-
 
         public void setX(int x) {
                 this.x = x;
         }
 
-
-
         public int getY() {
                 return y;
         }
-
-
 
         public void setY(int y) {
                 this.y = y;
         }
 
-
-
         public int getHeight() {
                 return height;
         }
-
-
 
         public void setHeight(int height) {
                 this.height = height;
         }
 
-
-
         public int getWidth() {
                 return width;
         }
-
-
 
         public void setWidth(int width) {
                 this.width = width;
