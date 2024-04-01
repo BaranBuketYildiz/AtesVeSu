1d217888f7a8440f48212f3b35bbeacb6d4a3c7d
BaranBuketYildiz <126967473+BaranBuketYildiz@users.noreply.github.com>
2024-04-01 22:15:38 +0300

    A shortcut that enables both players to move simultaneously when pressed.

--- GamePanel.java
+++ GamePanel.java
@@ -1,14 +1,15 @@
 import java.awt.*;
-
 import javax.swing.*;
-
 import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class GamePanel extends JPanel implements KeyListener {
 
     Player p1;
     Player p2;
-
+    private List<Integer> keyPressed=new ArrayList<>();
     public GamePanel() {
         setPreferredSize(new Dimension(900, 1200));
         this.addKeyListener(this);
@@ -22,55 +23,63 @@ public class GamePanel extends JPanel implements KeyListener {
     public void keyPressed(KeyEvent e) {
         int keyCode = e.getKeyCode();
         System.out.println("sdasdasd");
-        switch (keyCode) {
-            case KeyEvent.VK_W:
-                if (p1.getY() > 0)
-                    p1.setY(p1.getY() - 10);
-                repaint();
-                break;
-            case KeyEvent.VK_A:
-                if (p1.getX() > 0)
-                    p1.setX(p1.getX() - 10);
-                repaint();
-                break;
-
-            case KeyEvent.VK_S:
-              if (p1.getY() + p1.getHeight() < getHeight())
-                    p1.setY(p1.getY() + 10);
-                repaint();
-                break;
-            case KeyEvent.VK_D:
-            if(p1.getX()+p1.getWidth()<getWidth())
-                    p1.setX(p1.getX() + 10);
-                repaint();
-                break;
-            case KeyEvent.VK_UP:
-                if (p2.getY() > 0)
-                    p2.setY(p2.getY() - 10);
-                repaint();
-                break;
-            case KeyEvent.VK_DOWN:
-            if(p2.getY()+p2.getHeight()<getHeight())
-                p2.setY(p2.getY() + 10);
-                repaint();
-                break;
-
-            case KeyEvent.VK_LEFT:
-            if(p2.getX()>0)
-                p2.setX(p2.getX() - 10);
-                repaint();
-                break;
-            case KeyEvent.VK_RIGHT:
-            if(p2.getX()+p2.getWidth()<getWidth())
-                p2.setX(p2.getX() + 10);
-                repaint();
-                break;
+        if(!keyPressed.contains(keyCode)){
+                keyPressed.add(keyCode);
         }
+        for (Integer key : keyPressed) {
+            switch (key) {
+                case KeyEvent.VK_W:
+                    if (p1.getY() > 0)
+                        p1.setY(p1.getY() - 10);
+                    repaint();
+                    break;
+                case KeyEvent.VK_A:
+                    if (p1.getX() > 0)
+                        p1.setX(p1.getX() - 10);
+                    repaint();
+                    break;
+    
+                case KeyEvent.VK_S:
+                  if (p1.getY() + p1.getHeight() < getHeight())
+                        p1.setY(p1.getY() + 10);
+                    repaint();
+                    break;
+                case KeyEvent.VK_D:
+                if(p1.getX()+p1.getWidth()<getWidth())
+                        p1.setX(p1.getX() + 10);
+                    repaint();
+                    break;
+                case KeyEvent.VK_UP:
+                    if (p2.getY() > 0)
+                        p2.setY(p2.getY() - 10);
+                    repaint();
+                    break;
+                case KeyEvent.VK_DOWN:
+                if(p2.getY()+p2.getHeight()<getHeight())
+                    p2.setY(p2.getY() + 10);
+                    repaint();
+                    break;
+    
+                case KeyEvent.VK_LEFT:
+                if(p2.getX()>0)
+                    p2.setX(p2.getX() - 10);
+                    repaint();
+                    break;
+                case KeyEvent.VK_RIGHT:
+                if(p2.getX()+p2.getWidth()<getWidth())
+                    p2.setX(p2.getX() + 10);
+                    repaint();
+                    break;
+            }
+        }
+        
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
         // TODO Auto-generated method stub
+        int keyCode=e.getKeyCode();
+        keyPressed.remove(Integer.valueOf(keyCode));
 
     }
 
